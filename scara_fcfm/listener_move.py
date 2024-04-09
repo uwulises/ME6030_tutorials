4801e98548fa3096ed725b513c6019e67c573a17
uwulises <ulises.campodonico@ug.uchile.cl>
2024-04-09 10:52:14 -0400

    add z move

--- scara_fcfm/listener_move.py
+++ scara_fcfm/listener_move.py
@@ -24,19 +24,23 @@ def move_robot():
     data = json.loads(data)
     x_move = data['x']
     y_move = data['y']
+    z_move = data['z']
     #convert data to float
     x_move = float(x_move)
     y_move = float(y_move)
+    z_move = float(z_move)
     #check limits of x and y
     if x_move < -200 or x_move > 200:
         return jsonify({'status': 'error', 'message': 'x must be between 0 and 400'})
     if y_move < 400 or y_move > 600:
-        return jsonify({'status': 'error', 'message': 'y must be between 0 and 400'})
+        return jsonify({'status': 'error', 'message': 'y must be between 400 and 600'})
+    if z_move < 30 or z_move > 170:
+        return jsonify({'status': 'error', 'message': 'z must be between 30 and 170'})
 
-    #nelen.move(x_move, y_move, 100.0, 'right')
-    print("Moving to x: ", x_move, " y: ", y_move)
+    nelen.move(x_move, y_move, z_move, 'right')
+    print("Moving to x: ", x_move, " y: ", y_move, " z: ", z_move)
     # Return a success response
-    return jsonify({'status': 'success', 'message': 'Robot moved successfully to x: ' + str(x_move) + ' y: ' + str(y_move)})
+    return jsonify({'status': 'success', 'message': 'Robot moved successfully to x: ' + str(x_move) + ' y: ' + str(y_move) + ' z: ' + str(z_move)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
